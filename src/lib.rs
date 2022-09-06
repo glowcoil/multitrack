e3e959235bb823bd04a28b747149c98fcd0a8062
glowcoil <micahrjohnston@gmail.com>
2022-09-05 20:07:03 -0500

    implement integer types and remaining mask types

--- src/lib.rs
+++ src/lib.rs
@@ -13,11 +13,26 @@ mod scalar;
 #[allow(non_camel_case_types)]
 pub trait Arch
 where
-    Self::m32: Select<Self::f32> + Select<Self::m32>,
-    Self::m64: Select<Self::f64> + Select<Self::m64>,
+    Self::m8: Select<Self::i8> + Select<Self::u8> + Select<Self::m8>,
+    Self::m16: Select<Self::i16> + Select<Self::u16> + Select<Self::m16>,
+    Self::m32: Select<Self::f32> + Select<Self::i32> + Select<Self::u32> + Select<Self::m32>,
+    Self::m64: Select<Self::f64> + Select<Self::i64> + Select<Self::u64> + Select<Self::m64>,
 {
+    type u8: Simd<Arch = Self, Elem = u8> + Num;
+    type u16: Simd<Arch = Self, Elem = u16> + Num;
+    type u32: Simd<Arch = Self, Elem = u32> + Num;
+    type u64: Simd<Arch = Self, Elem = u64> + Num;
+
+    type i8: Simd<Arch = Self, Elem = i8> + Num;
+    type i16: Simd<Arch = Self, Elem = i16> + Num;
+    type i32: Simd<Arch = Self, Elem = i32> + Num;
+    type i64: Simd<Arch = Self, Elem = i64> + Num;
+
     type f32: Simd<Arch = Self, Elem = f32> + Num;
     type f64: Simd<Arch = Self, Elem = f64> + Num;
+
+    type m8: Simd<Arch = Self, Elem = m8> + Mask;
+    type m16: Simd<Arch = Self, Elem = m16> + Mask;
     type m32: Simd<Arch = Self, Elem = m32> + Mask;
     type m64: Simd<Arch = Self, Elem = m64> + Mask;
 }
--- src/scalar.rs
+++ src/scalar.rs
@@ -4,21 +4,13 @@ use crate::mask::*;
 use crate::{Arch, LanesEq, LanesOrd, Mask, Num, Select, Simd};
 
 use core::fmt::{self, Debug};
+use core::num::Wrapping;
 use core::ops::{
     Add, AddAssign, Div, DivAssign, Mul, MulAssign, Neg, Rem, RemAssign, Sub, SubAssign,
 };
 use core::ops::{BitAnd, BitAndAssign, BitOr, BitOrAssign, BitXor, BitXorAssign, Not};
 use core::ops::{Index, IndexMut};
 use core::slice;
-
-pub struct Scalar;
-
-impl Arch for Scalar {
-    type f32 = f32x1;
-    type f64 = f64x1;
-    type m32 = m32x1;
-    type m64 = m64x1;
-}
 
 macro_rules! scalar_type {
     ($scalar:ident, $inner:ident, $mask:ident) => {
@@ -96,6 +88,87 @@ macro_rules! scalar_type {
         impl Debug for $scalar {
             fn fmt(&self, fmt: &mut fmt::Formatter<'_>) -> fmt::Result {
                 fmt.debug_list().entry(&self.0).finish()
+            }
+        }
+    };
+}
+
+macro_rules! wrapping_scalar_type {
+    ($scalar:ident, $inner:ident, $mask:ident) => {
+        #[derive(Copy, Clone, Default)]
+        #[repr(transparent)]
+        pub struct $scalar(Wrapping<$inner>);
+
+        impl Simd for $scalar {
+            type Arch = Scalar;
+            type Elem = $inner;
+
+            const LANES: usize = 1;
+
+            fn new(elem: Self::Elem) -> Self {
+                $scalar(Wrapping(elem))
+            }
+
+            fn as_slice(&self) -> &[Self::Elem] {
+                slice::from_ref(&self.0 .0)
+            }
+
+            fn as_mut_slice(&mut self) -> &mut [Self::Elem] {
+                slice::from_mut(&mut self.0 .0)
+            }
+
+            fn from_slice(slice: &[Self::Elem]) -> Self {
+                Self::new(slice[0])
+            }
+
+            fn write_to_slice(&self, slice: &mut [Self::Elem]) {
+                slice[0] = self.0 .0;
+            }
+
+            fn align_slice(slice: &[Self::Elem]) -> (&[Self::Elem], &[Self], &[Self::Elem]) {
+                unsafe { slice.align_to::<Self>() }
+            }
+
+            fn align_mut_slice(
+                slice: &mut [Self::Elem],
+            ) -> (&mut [Self::Elem], &mut [Self], &mut [Self::Elem]) {
+                unsafe { slice.align_to_mut::<Self>() }
+            }
+        }
+
+        impl LanesEq for $scalar {
+            type Output = $mask;
+
+            fn eq(&self, other: &$scalar) -> $mask {
+                $mask((self.0 == other.0).into())
+            }
+        }
+
+        impl LanesOrd for $scalar {
+            fn lt(&self, other: &$scalar) -> $mask {
+                $mask((self.0 < other.0).into())
+            }
+        }
+
+        impl Index<usize> for $scalar {
+            type Output = $inner;
+
+            fn index(&self, index: usize) -> &$inner {
+                assert!(index == 0);
+                &self.0 .0
+            }
+        }
+
+        impl IndexMut<usize> for $scalar {
+            fn index_mut(&mut self, index: usize) -> &mut $inner {
+                assert!(index == 0);
+                &mut self.0 .0
+            }
+        }
+
+        impl Debug for $scalar {
+            fn fmt(&self, fmt: &mut fmt::Formatter<'_>) -> fmt::Result {
+                fmt.debug_list().entry(&self.0 .0).finish()
             }
         }
     };
@@ -253,12 +326,56 @@ macro_rules! impl_mask {
     };
 }
 
+pub struct Scalar;
+
+impl Arch for Scalar {
+    type f32 = f32x1;
+    type f64 = f64x1;
+
+    type u8 = u8x1;
+    type u16 = u16x1;
+    type u32 = u32x1;
+    type u64 = u64x1;
+
+    type i8 = i8x1;
+    type i16 = i16x1;
+    type i32 = i32x1;
+    type i64 = i64x1;
+
+    type m8 = m8x1;
+    type m16 = m16x1;
+    type m32 = m32x1;
+    type m64 = m64x1;
+}
+
 scalar_type! { f32x1, f32, m32x1 }
 scalar_type! { f64x1, f64, m64x1 }
 impl_num! { f64x1 }
 impl_num! { f32x1 }
 
+wrapping_scalar_type! { u8x1, u8, m8x1 }
+wrapping_scalar_type! { u16x1, u16, m16x1 }
+wrapping_scalar_type! { u32x1, u32, m32x1 }
+wrapping_scalar_type! { u64x1, u64, m64x1 }
+impl_num! { u8x1 }
+impl_num! { u16x1 }
+impl_num! { u32x1 }
+impl_num! { u64x1 }
+
+wrapping_scalar_type! { i8x1, i8, m8x1 }
+wrapping_scalar_type! { i16x1, i16, m16x1 }
+wrapping_scalar_type! { i32x1, i32, m32x1 }
+wrapping_scalar_type! { i64x1, i64, m64x1 }
+impl_num! { i8x1 }
+impl_num! { i16x1 }
+impl_num! { i32x1 }
+impl_num! { i64x1 }
+
+scalar_type! { m8x1, m8, m8x1 }
+scalar_type! { m16x1, m16, m16x1 }
 scalar_type! { m32x1, m32, m32x1 }
 scalar_type! { m64x1, m64, m64x1 }
-impl_mask! { m64x1, { f64x1, m64x1 } }
-impl_mask! { m32x1, { f32x1, m32x1 } }
+impl_mask! { m8x1, { u8x1, i8x1, m8x1 } }
+impl_mask! { m16x1, { u16x1, i16x1, m16x1 } }
+impl_mask! { m32x1, { u32x1, i32x1, m32x1, f32x1 } }
+impl_mask! { m64x1, { u64x1, i64x1, m64x1, f64x1 } }
