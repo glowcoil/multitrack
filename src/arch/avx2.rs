13226f9595f50d1bb673aea369887f30ab7278da
micah <micah@glowcoil.com>
2022-09-08 23:34:34 -0500

    avx2::f64x4 and m64x4

--- src/arch/avx2.rs
+++ src/arch/avx2.rs
@@ -216,6 +216,204 @@ impl Neg for f32x8 {
 
 #[derive(Copy, Clone)]
 #[repr(transparent)]
+pub struct f64x4(__m256d);
+
+impl Simd for f64x4 {
+    type Elem = f64;
+
+    const LANES: usize = 4;
+
+    fn new(elem: Self::Elem) -> Self {
+        unsafe { f64x4(_mm256_set1_pd(elem)) }
+    }
+
+    fn as_slice(&self) -> &[Self::Elem] {
+        unsafe { slice::from_raw_parts(self as *const Self as *const Self::Elem, Self::LANES) }
+    }
+
+    fn as_mut_slice(&mut self) -> &mut [Self::Elem] {
+        unsafe { slice::from_raw_parts_mut(self as *mut Self as *mut Self::Elem, Self::LANES) }
+    }
+
+    fn from_slice(slice: &[Self::Elem]) -> Self {
+        assert!(slice.len() == Self::LANES);
+        unsafe { f64x4(_mm256_loadu_pd(slice.as_ptr())) }
+    }
+
+    fn write_to_slice(&self, slice: &mut [Self::Elem]) {
+        assert!(slice.len() == Self::LANES);
+        unsafe {
+            _mm256_storeu_pd(slice.as_mut_ptr(), self.0);
+        }
+    }
+
+    fn align_slice(slice: &[Self::Elem]) -> (&[Self::Elem], &[Self], &[Self::Elem]) {
+        unsafe { slice.align_to::<Self>() }
+    }
+
+    fn align_mut_slice(
+        slice: &mut [Self::Elem],
+    ) -> (&mut [Self::Elem], &mut [Self], &mut [Self::Elem]) {
+        unsafe { slice.align_to_mut::<Self>() }
+    }
+}
+
+impl Debug for f64x4 {
+    fn fmt(&self, fmt: &mut fmt::Formatter<'_>) -> fmt::Result {
+        Debug::fmt(self.as_slice(), fmt)
+    }
+}
+
+impl Default for f64x4 {
+    fn default() -> Self {
+        unsafe { mem::zeroed() }
+    }
+}
+
+impl LanesEq for f64x4 {
+    type Output = m64x4;
+
+    fn eq(&self, other: &Self) -> Self::Output {
+        unsafe {
+            let res = _mm256_cmp_pd(self.0, other.0, _CMP_EQ_OQ);
+            m64x4(_mm256_castpd_si256(res))
+        }
+    }
+
+    fn ne(&self, other: &Self) -> Self::Output {
+        unsafe {
+            let res = _mm256_cmp_pd(self.0, other.0, _CMP_NEQ_OQ);
+            m64x4(_mm256_castpd_si256(res))
+        }
+    }
+}
+
+impl Index<usize> for f64x4 {
+    type Output = <Self as Simd>::Elem;
+
+    fn index(&self, index: usize) -> &Self::Output {
+        &self.as_slice()[index]
+    }
+}
+
+impl IndexMut<usize> for f64x4 {
+    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+        &mut self.as_mut_slice()[index]
+    }
+}
+
+impl Num for f64x4 {}
+
+impl LanesOrd for f64x4 {
+    fn lt(&self, other: &Self) -> Self::Output {
+        unsafe {
+            let res = _mm256_cmp_pd(self.0, other.0, _CMP_LT_OQ);
+            m64x4(_mm256_castpd_si256(res))
+        }
+    }
+
+    fn le(&self, other: &Self) -> Self::Output {
+        unsafe {
+            let res = _mm256_cmp_pd(self.0, other.0, _CMP_LE_OQ);
+            m64x4(_mm256_castpd_si256(res))
+        }
+    }
+
+    fn gt(&self, other: &Self) -> Self::Output {
+        unsafe {
+            let res = _mm256_cmp_pd(self.0, other.0, _CMP_GT_OQ);
+            m64x4(_mm256_castpd_si256(res))
+        }
+    }
+
+    fn ge(&self, other: &Self) -> Self::Output {
+        unsafe {
+            let res = _mm256_cmp_pd(self.0, other.0, _CMP_GE_OQ);
+            m64x4(_mm256_castpd_si256(res))
+        }
+    }
+}
+
+impl Add for f64x4 {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self {
+        unsafe { f64x4(_mm256_add_pd(self.0, rhs.0)) }
+    }
+}
+
+impl AddAssign for f64x4 {
+    fn add_assign(&mut self, rhs: Self) {
+        *self = *self + rhs;
+    }
+}
+
+impl Sub for f64x4 {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self {
+        unsafe { f64x4(_mm256_sub_pd(self.0, rhs.0)) }
+    }
+}
+
+impl SubAssign for f64x4 {
+    fn sub_assign(&mut self, rhs: Self) {
+        *self = *self - rhs;
+    }
+}
+
+impl Mul for f64x4 {
+    type Output = Self;
+
+    fn mul(self, rhs: Self) -> Self {
+        unsafe { f64x4(_mm256_mul_pd(self.0, rhs.0)) }
+    }
+}
+
+impl MulAssign for f64x4 {
+    fn mul_assign(&mut self, rhs: Self) {
+        *self = *self * rhs;
+    }
+}
+
+impl Div for f64x4 {
+    type Output = Self;
+
+    fn div(self, rhs: Self) -> Self {
+        unsafe { f64x4(_mm256_div_pd(self.0, rhs.0)) }
+    }
+}
+
+impl DivAssign for f64x4 {
+    fn div_assign(&mut self, rhs: Self) {
+        *self = *self / rhs;
+    }
+}
+
+impl Rem for f64x4 {
+    type Output = Self;
+
+    fn rem(self, rhs: Self) -> Self {
+        unimplemented!()
+    }
+}
+
+impl RemAssign for f64x4 {
+    fn rem_assign(&mut self, rhs: Self) {
+        unimplemented!()
+    }
+}
+
+impl Neg for f64x4 {
+    type Output = Self;
+
+    fn neg(self) -> Self {
+        unsafe { f64x4(_mm256_sub_pd(_mm256_set1_pd(0.0), self.0)) }
+    }
+}
+
+#[derive(Copy, Clone)]
+#[repr(transparent)]
 pub struct m32x8(__m256i);
 
 impl Mask for m32x8 {}
@@ -275,6 +473,71 @@ impl Select<f32x8> for m32x8 {
         unsafe {
             let mask = _mm256_castsi256_ps(self.0);
             f32x8(_mm256_blendv_ps(if_false.0, if_true.0, mask))
+        }
+    }
+}
+
+#[derive(Copy, Clone)]
+#[repr(transparent)]
+pub struct m64x4(__m256i);
+
+impl Mask for m64x4 {}
+
+impl BitAnd for m64x4 {
+    type Output = Self;
+
+    fn bitand(self, rhs: Self) -> Self::Output {
+        unsafe { m64x4(_mm256_and_si256(self.0, rhs.0)) }
+    }
+}
+
+impl BitAndAssign for m64x4 {
+    fn bitand_assign(&mut self, rhs: Self) {
+        *self = *self & rhs;
+    }
+}
+
+impl BitOr for m64x4 {
+    type Output = Self;
+
+    fn bitor(self, rhs: Self) -> Self::Output {
+        unsafe { m64x4(_mm256_or_si256(self.0, rhs.0)) }
+    }
+}
+
+impl BitOrAssign for m64x4 {
+    fn bitor_assign(&mut self, rhs: Self) {
+        *self = *self | rhs;
+    }
+}
+
+impl BitXor for m64x4 {
+    type Output = Self;
+
+    fn bitxor(self, rhs: Self) -> Self::Output {
+        unsafe { m64x4(_mm256_xor_si256(self.0, rhs.0)) }
+    }
+}
+
+impl BitXorAssign for m64x4 {
+    fn bitxor_assign(&mut self, rhs: Self) {
+        *self = *self ^ rhs;
+    }
+}
+
+impl Not for m64x4 {
+    type Output = Self;
+
+    fn not(self) -> Self::Output {
+        unsafe { m64x4(_mm256_andnot_si256(self.0, _mm256_set1_epi8(!0))) }
+    }
+}
+
+impl Select<f64x4> for m64x4 {
+    fn select(self, if_true: f64x4, if_false: f64x4) -> f64x4 {
+        unsafe {
+            let mask = _mm256_castsi256_pd(self.0);
+            f64x4(_mm256_blendv_pd(if_false.0, if_true.0, mask))
         }
     }
 }
