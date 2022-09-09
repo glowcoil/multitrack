629fe1bc46ac2066de343ccda2b46c4d9b5807b4
micah <micah@glowcoil.com>
2022-09-08 23:26:51 -0500

    implement Num for avx2::f32x8

--- src/arch/avx2.rs
+++ src/arch/avx2.rs
@@ -1,6 +1,6 @@
 #![allow(non_camel_case_types)]
 
-use crate::{LanesEq, Mask, Select, Simd};
+use crate::{LanesEq, LanesOrd, Mask, Num, Select, Simd};
 
 #[cfg(target_arch = "x86")]
 use std::arch::x86::*;
@@ -8,6 +8,9 @@ use std::arch::x86::*;
 use std::arch::x86_64::*;
 
 use core::fmt::{self, Debug};
+use core::ops::{
+    Add, AddAssign, Div, DivAssign, Mul, MulAssign, Neg, Rem, RemAssign, Sub, SubAssign,
+};
 use core::ops::{BitAnd, BitAndAssign, BitOr, BitOrAssign, BitXor, BitXorAssign, Not};
 use core::ops::{Index, IndexMut};
 use std::mem;
@@ -98,6 +101,116 @@ impl Index<usize> for f32x8 {
 impl IndexMut<usize> for f32x8 {
     fn index_mut(&mut self, index: usize) -> &mut Self::Output {
         &mut self.as_mut_slice()[index]
+    }
+}
+
+impl Num for f32x8 {}
+
+impl LanesOrd for f32x8 {
+    fn lt(&self, other: &Self) -> Self::Output {
+        unsafe {
+            let res = _mm256_cmp_ps(self.0, other.0, _CMP_LT_OQ);
+            m32x8(_mm256_castps_si256(res))
+        }
+    }
+
+    fn le(&self, other: &Self) -> Self::Output {
+        unsafe {
+            let res = _mm256_cmp_ps(self.0, other.0, _CMP_LE_OQ);
+            m32x8(_mm256_castps_si256(res))
+        }
+    }
+
+    fn gt(&self, other: &Self) -> Self::Output {
+        unsafe {
+            let res = _mm256_cmp_ps(self.0, other.0, _CMP_GT_OQ);
+            m32x8(_mm256_castps_si256(res))
+        }
+    }
+
+    fn ge(&self, other: &Self) -> Self::Output {
+        unsafe {
+            let res = _mm256_cmp_ps(self.0, other.0, _CMP_GE_OQ);
+            m32x8(_mm256_castps_si256(res))
+        }
+    }
+}
+
+impl Add for f32x8 {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self {
+        unsafe { f32x8(_mm256_add_ps(self.0, rhs.0)) }
+    }
+}
+
+impl AddAssign for f32x8 {
+    fn add_assign(&mut self, rhs: Self) {
+        *self = *self + rhs;
+    }
+}
+
+impl Sub for f32x8 {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self {
+        unsafe { f32x8(_mm256_sub_ps(self.0, rhs.0)) }
+    }
+}
+
+impl SubAssign for f32x8 {
+    fn sub_assign(&mut self, rhs: Self) {
+        *self = *self - rhs;
+    }
+}
+
+impl Mul for f32x8 {
+    type Output = Self;
+
+    fn mul(self, rhs: Self) -> Self {
+        unsafe { f32x8(_mm256_mul_ps(self.0, rhs.0)) }
+    }
+}
+
+impl MulAssign for f32x8 {
+    fn mul_assign(&mut self, rhs: Self) {
+        *self = *self * rhs;
+    }
+}
+
+impl Div for f32x8 {
+    type Output = Self;
+
+    fn div(self, rhs: Self) -> Self {
+        unsafe { f32x8(_mm256_div_ps(self.0, rhs.0)) }
+    }
+}
+
+impl DivAssign for f32x8 {
+    fn div_assign(&mut self, rhs: Self) {
+        *self = *self / rhs;
+    }
+}
+
+impl Rem for f32x8 {
+    type Output = Self;
+
+    fn rem(self, rhs: Self) -> Self {
+        unimplemented!()
+    }
+}
+
+impl RemAssign for f32x8 {
+    fn rem_assign(&mut self, rhs: Self) {
+        unimplemented!()
+    }
+}
+
+impl Neg for f32x8 {
+    type Output = Self;
+
+    fn neg(self) -> Self {
+        unsafe { f32x8(_mm256_sub_ps(_mm256_set1_ps(0.0), self.0)) }
     }
 }
 
