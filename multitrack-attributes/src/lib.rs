d2ec4bc039dee2e7bde4e4d658852f26a52813f5
glowcoil <micahrjohnston@gmail.com>
2022-10-10 14:20:45 -0500

    factor out reusable parts of #[specialize] macro
    Factor out parts of the #[specialize] macro that will also be used from
    the #[dispatch] macro.

--- multitrack-attributes/src/lib.rs
+++ multitrack-attributes/src/lib.rs
@@ -1,96 +1,139 @@
 use proc_macro::TokenStream;
+use proc_macro2::TokenStream as TokenStream2;
 use quote::{format_ident, quote};
 use syn::{
-    parse_macro_input, spanned::Spanned, Error, FnArg, GenericParam, ItemFn, Pat, TypeParamBound,
+    parse_macro_input, spanned::Spanned, Error, FnArg, GenericParam, Ident, ItemFn, Pat,
+    TypeParamBound,
 };
 
-#[proc_macro_attribute]
-pub fn specialize(_attr: TokenStream, input: TokenStream) -> TokenStream {
-    let func = parse_macro_input!(input as ItemFn);
+struct FnInfo<'a> {
+    func: &'a ItemFn,
+    generic_idents: Vec<&'a Ident>,
+    generic_params_no_arch: Vec<&'a GenericParam>,
+    generic_idents_no_arch: Vec<&'a Ident>,
+    arg_idents: Vec<&'a Ident>,
+    arg_types: Vec<Ident>,
+    arg_fields: Vec<Ident>,
+    arch_ident: &'a Ident,
+}
 
-    let attrs = &func.attrs;
-    let vis = &func.vis;
-    let sig = &func.sig;
+impl<'a> FnInfo<'a> {
+    fn from_fn(func: &ItemFn) -> Result<FnInfo, Error> {
+        let mut arch_ident = None;
 
-    let block = &func.block;
-
-    let mut arch_ident = None;
-
-    let mut generic_idents = Vec::new();
-    let mut generic_params_no_arch = Vec::new();
-    let mut generic_idents_no_arch = Vec::new();
-    for param in &sig.generics.params {
-        match param {
-            GenericParam::Type(ty) => {
-                // Look for <_: Arch> generic parameter
-                let mut found_arch = false;
-                if arch_ident.is_none() {
-                    for bound in &ty.bounds {
-                        if let TypeParamBound::Trait(tr) = bound {
-                            if let Some(last) = tr.path.segments.last() {
-                                if last.ident.to_string() == "Arch" {
-                                    arch_ident = Some(&ty.ident);
-                                    found_arch = true;
+        let mut generic_idents = Vec::new();
+        let mut generic_params_no_arch = Vec::new();
+        let mut generic_idents_no_arch = Vec::new();
+        for param in &func.sig.generics.params {
+            match param {
+                GenericParam::Type(ty) => {
+                    // Look for <_: Arch> generic parameter
+                    let mut found_arch = false;
+                    if arch_ident.is_none() {
+                        for bound in &ty.bounds {
+                            if let TypeParamBound::Trait(tr) = bound {
+                                if let Some(last) = tr.path.segments.last() {
+                                    if last.ident.to_string() == "Arch" {
+                                        arch_ident = Some(&ty.ident);
+                                        found_arch = true;
+                                    }
                                 }
                             }
                         }
                     }
+
+                    generic_idents.push(&ty.ident);
+
+                    if !found_arch {
+                        generic_params_no_arch.push(param);
+                        generic_idents_no_arch.push(&ty.ident);
+                    }
                 }
+                GenericParam::Const(_) => {
+                    return Err(Error::new(
+                        func.sig.generics.span(),
+                        "const generics not supported",
+                    ));
+                }
+                GenericParam::Lifetime(_) => {}
+            };
+        }
 
-                generic_idents.push(&ty.ident);
+        if arch_ident.is_none() {
+            return Err(Error::new(
+                func.sig.generics.span(),
+                "could not find Arch parameter",
+            ));
+        }
+        let arch_ident = arch_ident.unwrap();
 
-                if !found_arch {
-                    generic_params_no_arch.push(param);
-                    generic_idents_no_arch.push(&ty.ident);
+        let mut arg_idents = Vec::with_capacity(func.sig.inputs.len());
+        let mut arg_types = Vec::with_capacity(func.sig.inputs.len());
+        let mut arg_fields = Vec::with_capacity(func.sig.inputs.len());
+        for (i, arg) in func.sig.inputs.iter().enumerate() {
+            match arg {
+                FnArg::Receiver(_) => {
+                    return Err(Error::new(
+                        func.span(),
+                        "associated functions not supported",
+                    ));
+                }
+                FnArg::Typed(typed) => {
+                    if let Pat::Ident(ident) = &*typed.pat {
+                        arg_idents.push(&ident.ident);
+                        arg_types.push(format_ident!("I{}", i));
+                        arg_fields.push(format_ident!("_{}", i));
+                    } else {
+                        return Err(Error::new(typed.span(), "argument patterns not supported"));
+                    }
                 }
             }
-            GenericParam::Const(_) => {
-                return Error::new(func.sig.generics.span(), "const generics not supported")
-                    .into_compile_error()
-                    .into();
-            }
-            GenericParam::Lifetime(_) => {}
-        };
+        }
+
+        Ok(FnInfo {
+            func,
+            arch_ident,
+            generic_idents,
+            generic_params_no_arch,
+            generic_idents_no_arch,
+            arg_idents,
+            arg_types,
+            arg_fields,
+        })
     }
 
-    if arch_ident.is_none() {
-        return Error::new(func.sig.generics.span(), "could not find Arch parameter")
-            .into_compile_error()
-            .into();
-    }
-    let arch_ident = arch_ident.unwrap();
+    fn specialize(&self) -> TokenStream2 {
+        let attrs = &self.func.attrs;
+        let vis = &self.func.vis;
+        let sig = &self.func.sig;
 
-    let mut inner_sig = sig.clone();
-    inner_sig.ident = format_ident!("__inner");
+        let task = self.task();
+        let invoke = self.invoke();
 
-    let mut arg_types = Vec::with_capacity(sig.inputs.len());
-    let mut arg_fields = Vec::with_capacity(sig.inputs.len());
-    let mut arg_names = Vec::with_capacity(sig.inputs.len());
-    for (i, arg) in sig.inputs.iter().enumerate() {
-        arg_types.push(format_ident!("I{}", i));
-        arg_fields.push(format_ident!("_{}", i));
+        quote! {
+            #(#attrs)*
+            #vis #sig {
+                #task
 
-        match arg {
-            FnArg::Receiver(_) => {
-                return Error::new(func.span(), "associated functions not supported")
-                    .into_compile_error()
-                    .into();
-            }
-            FnArg::Typed(typed) => {
-                if let Pat::Ident(ident) = &*typed.pat {
-                    arg_names.push(ident);
-                } else {
-                    return Error::new(typed.span(), "argument patterns not supported")
-                        .into_compile_error()
-                        .into();
-                }
+                #invoke
             }
         }
     }
 
-    let result = quote! {
-        #(#attrs)*
-        #vis #sig {
+    fn task(&self) -> TokenStream2 {
+        let generic_idents = &self.generic_idents;
+        let generic_params_no_arch = &self.generic_params_no_arch;
+        let generic_idents_no_arch = &self.generic_idents_no_arch;
+        let arg_types = &self.arg_types;
+        let arg_fields = &self.arg_fields;
+        let arch_ident = &self.arch_ident;
+
+        let mut inner_sig = self.func.sig.clone();
+        inner_sig.ident = format_ident!("__inner");
+
+        let block = &self.func.block;
+
+        quote! {
             struct __Task<#(#generic_idents_no_arch,)* F, #(#arg_types,)*> {
                 #(#arg_fields: ::core::mem::ManuallyDrop<#arg_types>,)*
                 _f: F,
@@ -118,14 +161,32 @@ pub fn specialize(_attr: TokenStream, input: TokenStream) -> TokenStream {
 
             #[inline(always)]
             #inner_sig #block
+        }
+    }
 
+    fn invoke(&self) -> TokenStream2 {
+        let generic_idents = &self.generic_idents;
+        let generic_idents_no_arch = &self.generic_idents_no_arch;
+        let arch_ident = &self.arch_ident;
+        let arg_idents = &self.arg_idents;
+        let arg_fields = &self.arg_fields;
+
+        quote! {
             #arch_ident::invoke(__Task {
-                #(#arg_fields: ::core::mem::ManuallyDrop::new(#arg_names),)*
+                #(#arg_fields: ::core::mem::ManuallyDrop::new(#arg_idents),)*
                 _f: __inner::<#(#generic_idents,)*>,
                 _phantom: ::core::marker::PhantomData::<(#(#generic_idents_no_arch,)*)>,
             })
         }
-    };
+    }
+}
 
-    TokenStream::from(result)
+#[proc_macro_attribute]
+pub fn specialize(_attr: TokenStream, input: TokenStream) -> TokenStream {
+    let func = parse_macro_input!(input as ItemFn);
+
+    match FnInfo::from_fn(&func) {
+        Ok(info) => info.specialize().into(),
+        Err(err) => err.into_compile_error().into(),
+    }
 }
